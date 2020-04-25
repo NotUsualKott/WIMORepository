07bc2bbe938fc09d217143097526dee7d54a2969
NotUsualKott <58012354+NotUsualKott@users.noreply.github.com>
2020-04-25 12:49:59 +0300

    Quality Of Life Patch1

---
+++ WIMORepository/View/Windows/FuncWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WIMORepository.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для FuncWindow.xaml
+    /// </summary>
+    public partial class FuncWindow : Window
+    {
+        public FuncWindow()
+        {
+            InitializeComponent();
+            DataContext = new ViewModel.FuncWindowViewModel.FuncWindowViewModelClass();
+        }
+    }
+}
